4509edd5c3f341c894f92f0b35e94b89b0f2f5c5
ForeztNoize <gurk.svensson@gmail.com>
2021-07-03 17:17:17 +0200

    Temporary ToggleButton for content

--- src/Articles/ArticlesList.js
+++ src/Articles/ArticlesList.js
@@ -3,14 +3,30 @@ import { Component } from 'react';
 import ArticleData from '../Data/articles.json';
 import ArticlePicture from '../Data/article_pic.jpg';
 class ArticlesList extends Component{
+    constructor (props) {
+        super(props);
+        this.state = {
+            showContent: true
+        };
+        this.toggleContent = this.toggleContent.bind(this);
+    }
+
+    toggleContent(event){
+        event.preventDefault();
+        this.setState({
+            showContent: !this.state.showContent
+        })
+    }
     render(){
+        const {showContent} = this.state;
         return (
             <div id="articles">
                 {ArticleData.map((articleContent, index)=>{
-                    return  <div id="articleContent">
-                                <h1>{articleContent.title}</h1> 
+                    return  <div key={articleContent.id} id="articleContent">
+                                <h1 onClick={this.titleClick}>{articleContent.title}</h1> 
                                 <img src={ArticlePicture}/>
-                                <p>{articleContent.content}</p>    
+                                {showContent === true ? <p>{articleContent.content}</p> : ""} 
+                                <button onClick={this.toggleContent}>Toggle Content</button>  
                             </div>
                 })}
             </div>
