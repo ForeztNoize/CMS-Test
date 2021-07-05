4cded1b0d14cab69936e2101952fb7cfb529659f
ForeztNoize <gurk.svensson@gmail.com>
2021-07-05 10:40:39 +0200

    changed login reducer
    If you login in as admin two times you will no longer log out.

--- src/reducers/isLogged.js
+++ src/reducers/isLogged.js
@@ -1,7 +1,7 @@
 const loggedReducer = (state = false, action) => {
     switch(action.type){
         case 'SIGNED_IN':
-            return !state
+            return true;
         default:
             return state;
     }
